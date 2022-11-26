d75dab7255dfcb1f2f37794b85dc5b9a5a8ddfc1
Jim T <jimte145@student.liu.se>
2022-11-27 00:12:44 +0100

    Almost got MVP working

--- src/app/question-blocks/question-blocks.component.ts
+++ src/app/question-blocks/question-blocks.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import questionData from '../../assets/json/questions.json'
 
 @Component({
   selector: 'app-question-blocks',
   templateUrl: './question-blocks.component.html',
   styleUrls: ['./question-blocks.component.scss']
 })
-export class QuestionBlocksComponent {
-  
+
+export class QuestionBlocksComponent implements OnInit {
+  questions!: Question[];
+  qText: string = "";
+  answers: Answer[] = new Array<Answer>();
+
+  ngOnInit(): void {
+    this.questions = deserializeQuestions(questionData);
+
+    this.displayQuestion(1);
+  }
+
+  displayQuestion(id: number) {
+    let question: Question | undefined = this.questions.find((e) => {return e.id === id});
+    if (question !== undefined){
+      this.qText = question.questionBody;
+      this.answers = question.answers;
+    }
+  }
 }
+
+class Question{
+  id: number;
+  questionBody: string;
+  answers: Answer[];
+
+  constructor(id: number, question: string, answers: Answer[]){
+    this.id = id;
+    this.questionBody = question;
+    this.answers = answers;
+  }
+}
+
+class Answer{
+  answerBody: string;
+  leadsTo: number;
+
+  constructor(answer: string, leadsTo: number){
+    this.answerBody = answer;
+    this.leadsTo = leadsTo;
+  }
+}
+
+function deserializeQuestions(questionData: { id: number; questionBody: string; answers: { answerBody: string; leadsTo: number; }[]; }[]): Question[] {
+  let result: Question[] = Array<Question>();
+
+  questionData.forEach(element => {
+    result.push(new Question(element.id, element.questionBody, deSerializeAnswers(element.answers)));
+  });
+
+  return result;
+}
+function deSerializeAnswers(answers: { answerBody: string; leadsTo: number; }[]): Answer[] {
+  let result: Answer[] = Array<Answer>();
+
+  answers.forEach(element => {
+    result.push(new Answer(element.answerBody, element.leadsTo));
+  });
+
+  return result;
+}
+
