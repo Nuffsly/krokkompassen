29b54b12ebedf723c139884ef09d8778e2df9120
Jim Teräväinen <jimte145@student.liu.se>
2023-01-09 11:26:31 +0100

    Finished first version of styling and MD for rules
    Styled the rules in a more fitting way.
    Added MarkDown like syntax for line-break and bullet points in questions.json.

--- src/app/question-blocks/rules/rules.component.ts
+++ src/app/question-blocks/rules/rules.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output, ViewContainerRef } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewContainerRef } from '@angular/core';
 
 @Component({
   selector: 'app-rules',
@@ -6,14 +6,31 @@ import { Component, EventEmitter, Input, Output, ViewContainerRef } from '@angul
   styleUrls: ['./rules.component.scss']
 })
 
-export class RulesComponent {
+export class RulesComponent implements OnInit{
   @Input() _title: string = "";
   @Input() _body: string = "";
   @Input() _ViewContainerRef: ViewContainerRef | undefined = undefined;
   @Input() _isPhonePortrait: boolean = false;
   @Output() closeWindow: EventEmitter<ViewContainerRef> = new EventEmitter();
 
+  ngOnInit(): void {
+    this._body = formatBody(this._body);
+  }
+
   close(): void {
     this.closeWindow.emit(this._ViewContainerRef);
   }
+}
+
+function formatBody(text: string): string {
+  const bulletPattern: RegExp = /\* (.+?) \*\*/g;
+
+  text = text.replaceAll('\n', '<br>');
+  text = text.replaceAll(bulletPattern, bulletReplace);
+  return text;
+}
+
+function bulletReplace(match: string, content: string): string {
+  let result = "<ul><li>"+content+"</li></ul>"
+  return result
 }
